1b15d5d4477771d2476df45f49b19aa6f430ac80
Sean Heath <se@nheath.com>
2022-02-24 18:32:09 -0500

    updated server to use subcommand/gadget

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,32 +3,27 @@ package server
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/seandheath/gogogadget/internal/gadget"
 )
 
-var Gadget gadget.Gadget
+var Gadget gadget.Gadget = gadget.Gadget{
+	GadgetName:     "server",
+	GadgetSynopsis: "web server to share files in a directory",
+	GadgetUsage:    "server\n",
+	Run:            Run,
+	InitFlags:      initFlags,
+}
 var dir string
 var port string
 
-func init() {
-	Gadget = gadget.New("server", Run)
-
-	if len(os.Args) < 2 || os.Args[1] != "server" {
-		return
-	}
-	path, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	flag.StringVar(&dir, "dir", path, "directory to serve, defaults to current directory")
+func initFlags(f *flag.FlagSet) {
+	flag.StringVar(&dir, "dir", ".", "directory to serve, defaults to current directory")
 	flag.StringVar(&port, "port", "8080", "port to serve on, defaults to 8080")
 }
 
-func Run(args []string) {
+func Run() {
 	fmt.Printf("Starting server for directory %s on port %s\n\n", dir, port)
 	fs := http.FileServer(http.Dir(dir))
 	fmt.Println(http.ListenAndServe(":"+port, fs))
