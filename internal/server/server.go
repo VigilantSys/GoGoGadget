a167d82fc823a431219068e3228e6fb4d7dc175a
crusoe112 <crusoe112@yahoo.com>
2022-03-31 16:15:05 -0400

    Added upload feature to server

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,11 @@ package server
 import (
 	"flag"
 	"fmt"
+	"os"
+	"io"
 	"net/http"
+	"text/template"
+	"path/filepath"
 
 	"github.com/seandheath/gogogadget/internal/gadget"
 )
@@ -18,13 +22,66 @@ var Gadget gadget.Gadget = gadget.Gadget{
 var dir string
 var port string
 
+// Compile templates on start of the application
+var templates = template.Must(template.ParseFiles("./upload.html"))
+
 func initFlags(f *flag.FlagSet) {
-	flag.StringVar(&dir, "dir", ".", "directory to serve, defaults to current directory")
-	flag.StringVar(&port, "port", "8080", "port to serve on, defaults to 8080")
+	f.StringVar(&dir, "dir", ".", "directory to serve, defaults to current directory")
+	f.StringVar(&port, "port", "8080", "port to serve on, defaults to 8080")
+}
+
+// Display the named template
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create(filepath.Clean(fmt.Sprintf("%s/%s", dir, handler.Filename)))
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func uploadAction(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case "GET":
+			display(w, "upload", nil)
+		case "POST":
+			uploadFile(w, r)
+	}
 }
 
 func Run() {
 	fmt.Printf("Starting server for directory %s on port %s\n\n", dir, port)
+	http.HandleFunc("/upload", uploadAction)
 	fs := http.FileServer(http.Dir(dir))
-	fmt.Println(http.ListenAndServe(":"+port, fs))
+	http.Handle("/", fs)
+	fmt.Println(http.ListenAndServe(":"+port, nil))
 }
