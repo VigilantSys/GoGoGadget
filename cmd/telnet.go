24a3ec9d437909b8b989225f3cbc4e98ca33a794
crusoe112 <crusoe112@yahoo.com>
2022-07-16 18:52:49 -0400

    Simplified Telnet module

--- cmd/telnet.go
+++ cmd/telnet.go
@@ -30,14 +30,9 @@ package cmd
 
 import (
 	"fmt"
-	//"bufio"
-	"os"
-	"strings"
-	//"unicode/utf8"
 
 	"github.com/spf13/cobra"
 	"github.com/reiver/go-telnet"
-	"github.com/reiver/go-oi"
 )
 
 
@@ -47,16 +42,12 @@ var telnetCmd = &cobra.Command{
 	Short: "telnet client",
 	Long:  `Telnet provides a telnet client program.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		conn, err := telnet.DialTo(fmt.Sprintf("%s:%s", telnetAddress, telnetPort))
-		if err != nil {
-			fmt.Println(fmt.Errorf("Error connecting to telnet server: %w", err))
-			return
-		}
+		var caller telnet.Caller = telnet.StandardCaller
 
-		go Receive(conn)
-		c := make(chan bool, 1)
-		go Send(conn, c)
-		<-c
+		err := telnet.DialToAndCall(fmt.Sprintf("%s:%s", telnetAddress, telnetPort), caller)
+		if err != nil {
+			fmt.Errorf("error connecting to Telnet server: %w", err)
+		}
 
 	},
 }
@@ -70,64 +61,3 @@ func init() {
 	telnetCmd.Flags().StringVar(&telnetPort, "port", "23", "port to connect to")
 }
 
-func Receive(conn *telnet.Conn) {
-    //var buffer [1]byte
-    //recvData := buffer[:]
-    recvData := make([]byte, 1)
-    var n int
-    var err error
-
-    for {
-        n, err = conn.Read(recvData)
-        //fmt.Println("Bytes: ", n, "Data: ", recvData, string(recvData))
-        if n <= 0 || err != nil {
-            break
-        } else {
-		//r, _ :=utf8.DecodeRune(recvData)
-		//os.Stdout.Write(recvData)
-		if _, err := oi.LongWriteString(os.Stdout, string(recvData)); nil != err {
-		fmt.Println(err)
-	}
-        }
-    }
-}
-
-func Send(conn *telnet.Conn, c chan bool) {
-	//reader := bufio.NewReader(os.Stdin)
-	defer conn.Close()
-	running := ""
-	for {
-		var b []byte = make([]byte, 1)
-		os.Stdin.Read(b)
-		if string(b) == "\n" || string(b) == "\r" || string(b) == "\r\n" {
-			if strings.TrimSpace(running) == "quit" || strings.TrimSpace(running) == "exit" {
-				break
-			}
-			running = ""
-		}
-		running += string(b)
-
-		conn.Write(b)
-		/*
-		cmd, _ := reader.ReadString('\n')
-		if strings.TrimSpace(cmd) == "exit" || strings.TrimSpace(cmd) == "quit" {
-			break
-		}
-		*/
-    
-		/*
-		var commandBuffer []byte
-    		for _, char := range cmd {
-        		commandBuffer = append(commandBuffer, byte(char))
-    		}
-
-    		var crlfBuffer [2]byte = [2]byte{'\r', '\n'}
-    		crlf := crlfBuffer[:]
-
-    		conn.Write(commandBuffer)
-    		conn.Write(crlf)
-		*/
-		//conn.Write([]byte(cmd + "\r\n"))
-    	}
-	c <- true
-}
